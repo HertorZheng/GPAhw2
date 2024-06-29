579fe4b7ce6431deca60ea6f7860eedb4ba2374a
Hector <hectorzheng4@gmail.com>
2024-06-28 21:39:34 -0400

    Fix issues

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
-
-const mongoURI = 'mongodb+srv://hectorzheng4:Hector7126216!@hectorz.8d5rczo.mongodb.net/?retryWrites=true&w=majority&appName=HectorZ';
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost/gpa_calculator';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
@@ -13,6 +12,6 @@ app.use(express.static('public'));
 const courses = require('./routes/courses');
 app.use('/api/courses', courses);
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port ' + (process.env.PORT || 3000));
 });
